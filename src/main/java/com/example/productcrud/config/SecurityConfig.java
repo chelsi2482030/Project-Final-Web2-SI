2b6655f5a668387aafaefb61641fa38a21c4bd94
Chelsi Enatalia <2482030@unai.edu>
2026-04-27 16:32:05 +0700

    memperbaiki tampilan awal

--- src/main/java/com/example/productcrud/config/SecurityConfig.java
+++ src/main/java/com/example/productcrud/config/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig {
                 )
                 .formLogin(form -> form
                         .loginPage("/login")
-                        .defaultSuccessUrl("/products", true)
+                        .defaultSuccessUrl("/dashboard", true)
                         .permitAll()
                 )
                 .logout(logout -> logout
